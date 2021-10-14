7ee7b6c3597c86e41135a178fcca949932aaf811
p481026110 <p481026110@gmail.com>
2021-10-13 22:13:32 -0400

    archivos iniciales

---
+++ src/examen2.test.js
@@ -0,0 +1,10 @@
+import libreria, { GenerarMatriz } from './examen2.js';
+
+describe("EXAMEN2_INGESOFT", () => {
+
+  it("deberia", () => {
+    expect(0).toEqual(0);
+  });
+  
+});
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import libreria, { GenerarMatriz } from './examen2.js';
+
+const comandos = document.querySelector("#comandos-input");
+const form = document.querySelector("#controladorAuto-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  var posicionInicial = libreria.CapturarPosicionInicial(comandos.value);
+  var instrucciones = libreria.CapurarInstrucciones(comandos.value);
+  var grilla = libreria.CapurarTamañoGrilla(comandos.value);
+  //var comandosIngresados = comandos.value;
+  var posicionFinal = libreria.ControladorAuto(comandos.value);
+  
+  div.innerHTML = "<label>POSICION INICIAL ==> </label>" + posicionInicial + "<br>" + 
+                  "<label>INSTRUCCIONES ==> </label>" + instrucciones + "<br>" + 
+                  "<label>TAMAÑO GRILLA ==> </label>" +  grilla + "<br><br>" + 
+                  "<label>POSICION FINAL ==> </label>" + posicionFinal;
+});
